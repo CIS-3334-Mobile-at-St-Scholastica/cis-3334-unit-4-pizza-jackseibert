555391004f4190866649352f63c7ae06d0812efb
jackseibert <jackwseibert@gmail.com>
2019-02-07 12:45:40 -0600

    Step Four: Order a large pizza.

--- app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
+++ app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
@@ -60,13 +60,14 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     public void onClickOrder(View view) {
         // ****** For the Assignment, students need to add code here to get information from the UI widgets...
 
-        String orderDescription = "No orders yet";
+        String orderDescription = PizzaOrderSystem.OrderPizza("Pepperoni", "Large", false);
+        txtTotal.append(PizzaOrderSystem.getTotalBill().toString());
 
         // ****** For the Practice Activity, students need to call to OrderPizza here
         // ****** For the Assignment, students will modify the order to fit the type of pizza the user selects using the UI widgets
 
         //display a pop up message for a long period of time
-        Toast.makeText(getApplicationContext(), "You have ordered a "+orderDescription , Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "You have ordered a "+ orderDescription , Toast.LENGTH_LONG).show();
         // add this pizza to the textview the lists the pizzas
         txtPizzasOrdered.append(orderDescription+"\n");
 
